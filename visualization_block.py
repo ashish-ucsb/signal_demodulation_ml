5277241aafa0686035fb30986658a913ad4e13d8
Ashish Vyas <ashish@ucsb.edu>
2020-08-03 16:05:40 -0700

    Updated visualization block

--- visualization_block.py
+++ visualization_block.py
@@ -1,23 +1,18 @@
-import os
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2
 from scipy.io import loadmat
+from skimage.draw import line_aa
 
-def visualization_block(time_series, linewidth=10, output_shape=(28,28)):
+def visualization_block(time_series):
 	"""Visualization block: Converts time series to 2D image"""
-	fig = plt.figure()
-	fig.add_subplot(111)
-	fig.tight_layout(pad=0)
-	plt.ylim(0, 1)
-	plt.axis('off')
-	plt.plot(time_series, linewidth=linewidth)
-	fig.canvas.draw()
-	plt.close(fig)
-	img = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
-	img = img.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-	gray = cv2.bitwise_not(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
-	return cv2.resize(gray, output_shape)
+	n = len(time_series)
+	img = np.zeros((28, 28), dtype=np.uint8)
+	x = np.linspace(0, 27, n, endpoint=True, dtype=np.uint8)
+	y = (time_series*27).astype(int)
+	for i in range(n-1):
+	    rr, cc, val = line_aa(y[i], x[i], y[i+1], x[i+1])
+	    img[rr, cc] = val * 255
+	return np.flip(img, axis=0)
 
 if __name__ == '__main__':
 
